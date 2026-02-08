ecfb813bab93c9a42e8e7ad98fb7b9f9f96a9716
Gautier DI FOLCO <foss@difolco.dev>
2026-02-08 18:21:48 +0100

    feat: complete Pomodoro domain logic

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,8 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Main (main) where
 
 import Data.Time
--- import Data.Text
-
 import Data.Time.Clock.POSIX (POSIXTime)
 import Test.Hspec
 
@@ -14,44 +14,65 @@ spec =
   describe "Pomodoro" $ do
     describe "sequenceStatus" $ do
       it "when started at 0 asking at 5 minutes on a classical sequence, it should answer 20 minutes left" $
-        sequenceStatus (base $ minutes 0) (minutes 5) `shouldBe` Remaining (minutes 20)
+        sequenceStatus' (base $ minutes 0) (minutes 5)
+          `shouldBe` Remaining Work (minutes 20)
       it "when started at 0 asking at 15 minutes on a classical sequence, it should answer 10 minutes left" $
-        sequenceStatus (base $ minutes 0) (minutes 15) `shouldBe` Remaining (minutes 10)
+        sequenceStatus' (base $ minutes 0) (minutes 15)
+          `shouldBe` Remaining Work (minutes 10)
       it "when started at 0 asking at 30 minutes on a classical sequence, it should answer 5 minutes overtime" $
-        sequenceStatus (base $ minutes 0) (minutes 30) `shouldBe` Overtime (minutes 5)
-      it "when started at 0 asking at 30 minutes on a classical sequence, it should answer 5 minutes overtime" $
-        sequenceStatus (base $ minutes 0) (minutes 30) `shouldBe` Overtime (minutes 5)
+        sequenceStatus' (base $ minutes 0) (minutes 30)
+          `shouldBe` Overtime Work (minutes 5)
       it "when started at 10 minutes asking at 40 minutes on a classical sequence, it should answer 5 minutes overtime" $
-        sequenceStatus (base $ minutes 10) (minutes 40) `shouldBe` Overtime (minutes 5)
+        sequenceStatus' (base $ minutes 10) (minutes 40)
+          `shouldBe` Overtime Work (minutes 5)
       it "when started the break at 30 minutes asking at 34 minutes on a classical sequence, it should answer 1 minutes left" $
-        sequenceStatus (base (minutes 0) <> [StartedBreak (minutes 30)]) (minutes 34) `shouldBe` Remaining (minutes 1)
+        sequenceStatus' (base (minutes 0) <> [StartedBreak (minutes 30)]) (minutes 34)
+          `shouldBe` Remaining Break (minutes 1)
+      it "when starting a second work period at 32, at 50 minutes should remain 7 minutes" $
+        sequenceStatus' (base (minutes 0) <> [StartedBreak (minutes 26), StartedWork (minutes 32)]) (minutes 50)
+          `shouldBe` Remaining Work (minutes 7)
+      it "when starting a second work period at 32, at 50 minutes should remain 7 minutes" $
+        sequenceStatus' (base (minutes 0) <> [StartedBreak (minutes 26), StartedWork (minutes 32), StartedBreak (minutes 57), StartedWork (minutes 63), StartedBreak (minutes 89), StartedWork (minutes 95), StartedBreak (minutes 121)]) (minutes 122)
+          `shouldBe` Remaining Break (minutes 14)
 
 base :: POSIXTime -> [Event]
 base start =
-  [ Configured Config {pomodoro = minutes 25, shortPause = minutes 5, longPause = minutes 15},
-    StartedWork start
-  ]
+  [StartedWork start]
 
 minutes :: Int -> POSIXTime
 minutes = secondsToNominalDiffTime . (* 60) . fromIntegral
 
-data SequenceStatus = Remaining POSIXTime | Overtime POSIXTime
+data SequenceStatus = Remaining Stage POSIXTime | Overtime Stage POSIXTime
+  deriving stock (Eq, Show)
+
+data Stage = Work | Break
   deriving stock (Eq, Show)
 
 data Config = Config {pomodoro, shortPause, longPause :: POSIXTime}
 
-data Event = Configured Config | StartedWork POSIXTime | StartedBreak POSIXTime
+classicalConfig :: Config
+classicalConfig = Config{pomodoro = minutes 25, shortPause = minutes 5, longPause = minutes 15}
 
-sequenceStatus :: [Event] -> POSIXTime -> SequenceStatus
-sequenceStatus (Configured config:starts) queriedAt =
+data Event = StartedWork POSIXTime | StartedBreak POSIXTime
+
+sequenceStatus' = sequenceStatus classicalConfig
+
+sequenceStatus :: Config -> [Event] -> POSIXTime -> SequenceStatus
+sequenceStatus config starts queriedAt =
   if delta >= 0
     then
-      Remaining delta
+      Remaining currentStage delta
     else
-      Overtime $ -delta
-  where
-    delta = phaseDuration + begin - queriedAt
-    (begin, phaseDuration) =
-      case last starts of
-        StartedWork x -> (x, config.pomodoro)
-        StartedBreak x -> (x, config.shortPause)
+      Overtime currentStage $ -delta
+ where
+  delta = phaseDuration + begin - queriedAt
+  (begin, phaseDuration) =
+    case last starts of
+      StartedWork x -> (x, config.pomodoro)
+      StartedBreak x -> (x, if isLongPause then config.longPause else config.shortPause)
+  isLongPause = length (filter isWorkEvent starts) `mod` 4 == 0
+  currentStage = if isWorkEvent (last starts) then Work else Break
+  isWorkEvent =
+    \case
+      StartedWork _ -> True
+      StartedBreak _ -> False
